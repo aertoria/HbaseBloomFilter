587f3e046d3488d001ab12d4a7519fee322629ec
aertoria <castives@gmail.com>
2017-07-09 15:05:39 -0700

    Automatic commited. 10.0.0.111,10.3.41.118 Terminal

--- bloomfilter/src/main/java/org/apache/study/bloomfilter/BFCompare.java
+++ bloomfilter/src/main/java/org/apache/study/bloomfilter/BFCompare.java
@@ -7,7 +7,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.stream.Collectors;
 
+/**
+ * 
+ * @author ethan.wang
+ *
+ */
 public class BFCompare {
 	final public static int BFSize=10000;
 	
@@ -16,7 +22,7 @@ public class BFCompare {
 		Random rand = new Random();
 		List<Integer> randomList=new ArrayList<Integer>();
 		for(int i=0;i<BFCompare.BFSize;i++){
-			int n = rand.nextInt(10000) + 1;
+			int n = rand.nextInt(15000) + 1;
 			randomList.add(n);
 		}
 //		System.out.println(randomList);
@@ -26,9 +32,14 @@ public class BFCompare {
 		old_bf.checkHowManyCollide();
 
 		BF new_bf=new New_BF();
-		new_bf.setRange(Collections.min(randomList), Collections.max(randomList));
+		new_bf.setRange(Collections.min(randomList.stream().map(i -> FNV(i)).collect(Collectors.toList())), Collections.max(randomList.stream().map(i -> FNV(i)).collect(Collectors.toList())));
 		randomList.forEach(in -> new_bf.put(in));
 		new_bf.checkHowManyCollide();
+		
+		BF new_bf2=new New_BF2();
+		new_bf2.setRange(Collections.min(randomList.stream().map(i -> FNV(i)).collect(Collectors.toList())), Collections.max(randomList.stream().map(i -> FNV(i)).collect(Collectors.toList())));
+		randomList.forEach(in -> new_bf2.put(in));
+		new_bf2.checkHowManyCollide();
 
 	}
 	
@@ -48,7 +59,7 @@ interface BF{
 	
 	void checkHowManyCollide();
 	
-	default void setRange(int min, int max){}
+	default void setRange(int minHash, int maxHash){}
 }
 
 class Old_BF implements BF{
@@ -68,7 +79,7 @@ class Old_BF implements BF{
 		.reduce((r1,r2)->(r1+r2));
 		
 		float occupiedRate=(float)(sum.get())/(float)BFCompare.BFSize;
-		System.out.println("OLD BF. Occupied Rate: "+occupiedRate);
+		System.out.println("OLD BF. Occupied Rate: "+occupiedRate+" \t Collision Rate:"+ (1-occupiedRate));
 	}
 }
 
@@ -103,21 +114,52 @@ class New_BF implements BF{
 		.reduce((r1,r2)->(r1+r2));
 		
 		float occupiedRate=(float)(sum.get())/(float)BFCompare.BFSize;
-		System.out.println("NEW BF. Occupied Rate: "+occupiedRate);
+		System.out.println("NEW BF. Occupied Rate: "+occupiedRate+" \t Collision Rate:"+ (1-occupiedRate));
 	}
 	
 	@Override
-	public void setRange(int min, int max){
-		this.min=min;
-		this.max=max;
-		
-		int minHash=BFCompare.FNV(this.min);
+	public void setRange(int minHash, int maxHash){
 		this.delta=minHash%BFCompare.BFSize;//Align the min end with begining of the bf	
-		
-		int maxHash=BFCompare.FNV(this.max);
 		this.threshold=maxHash-delta-(maxHash-delta)%BFCompare.BFSize;
 		
 	}
 }
+	
+	
+
+
+
+class New_BF2 implements BF{
+	Byte[] bits;
+	int real_size=BFCompare.BFSize;
+	
+	public void put(int in) {
+		int hash=BFCompare.FNV(in);
+		bits[hash%real_size]=0;
+	}
+
+	public void checkHowManyCollide() {		
+		Optional<Integer> sum=new ArrayList<Byte>(Arrays.asList(bits))
+		.stream()
+		.map(bit -> {
+			return bit!=null?1:0;
+		})
+		.reduce((r1,r2)->(r1+r2));
+		
+		float occupiedRate=(float)(sum.get())/(float)BFCompare.BFSize;
+		System.out.println("NEW BF2. Occupied Rate: "+occupiedRate+" \t Collision Rate:"+ (1-occupiedRate));
+	}
+	
+	@Override
+	public void setRange(int minHash, int maxHash){
+		int diff=Math.abs(maxHash-minHash);
+		int estimateMuliplier=diff/BFCompare.BFSize;
+		if(estimateMuliplier>0)real_size=(diff)/estimateMuliplier;
+		System.out.println(real_size);
+		bits=new Byte[real_size];
+	}
+}
+
+
 
 
